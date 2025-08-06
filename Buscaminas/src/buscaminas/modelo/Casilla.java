24a6f2cef13f8a49de73465119e12da53e1e3343
dylan <dylan@DylanSoto>
2025-08-06 16:29:31 -0600

    primer commit DYLAN

---
+++ Buscaminas/src/buscaminas/modelo/Casilla.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package buscaminas.modelo;
+
+/**
+ *
+ * @author dylan
+ */
+public class Casilla {
+      private boolean tieneMina;
+    private boolean descubierta;
+    private boolean marcada;
+    private int minasCerca;
+
+    public Casilla() {
+        this.tieneMina = false;
+        this.descubierta = false;
+        this.marcada = false;
+        this.minasCerca = 0;
+    }
+ // Getters y Setters
+    public boolean tieneMina() {
+        return tieneMina;
+    }
+public void setMina(boolean mina) {
+        this.tieneMina = mina;
+    }
+public boolean estaDescubierta() {
+        return descubierta;
+    }
+ public void setDescubierta(boolean descubierta) {
+        this.descubierta = descubierta;
+    }
+ public boolean estaMarcada() {
+        return marcada;
+    }
+
+public void setMarcada(boolean marcada) {
+        this.marcada = marcada;
+    }
+ public int getMinasCerca() {
+        return minasCerca;
+    }
+ public void setMinasCerca(int minasCerca) {
+        this.minasCerca = minasCerca;
+    }
+
+}
